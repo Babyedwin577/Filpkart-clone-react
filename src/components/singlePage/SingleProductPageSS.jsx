6faf14ace57d704d5e144718496b149f58350eb7
Edwin Baby <edwinbaby305@gmail.com>
2025-09-21 21:05:14 +0530

    completed single product Page

--- src/components/singlePage/SingleProductPageSS.jsx
+++ src/components/singlePage/SingleProductPageSS.jsx
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from 'react'
+import bankCards from '../../config/BankOffers'
 import { useNavigate } from 'react-router-dom'
 import './singlepagess.css'
 export const SingleProductPageSS = ({product}) => {
@@ -20,7 +21,7 @@ export const SingleProductPageSS = ({product}) => {
         navigate(-1)
      }
   return (
-    <div>
+    <div style={{paddingBottom:"60px"}}>
         <div className='spss-head-wrap'>
             <div style={{margin:"12px 0 12px 8px",display:"flex"}}>
                 <div className='spss-backbtn-wrap' onClick={handleback}>
@@ -146,8 +147,109 @@ export const SingleProductPageSS = ({product}) => {
                         </div>
                     </div>
                 </div>
+                <div className='wd-part2-wrap'>
+                    <div className='add-item-head-wrap'>
+                        <div className='add-itemhead-left'>Bank offers</div>
+                    </div>
+                    <div className='spss-offercard-wrap'>
+                        {
+                            bankCards.map((column,index)=>(
+                                <div key={index} className='spss-offercard-columnwrap'>
+                                  {
+                                    column.map((item,idx)=>(
+                                        <div key={idx} className='spss-offercard-item'>
+                                            <div className='spss-offercard-sec-1'>
+                                                <img src={item.cardImage} alt='card-photo' height={24} width={24} />
+                                                <div className='bank-cardapply-wrap'>
+                                                    <div className='add-item-head-wrap' style={{padding:"0",width:"100%"}}>
+                                                        <div className='unlock-offer-precent'>{item.offer}</div>
+                                                        <div className='add-itemhead-right'>Apply</div>
+                                                    </div>
+                                                    <div className='spss-bankcardName'>{item.cardName}</div>
+                                                </div>
+                                            </div>
+                                            <div className='cardType-wrap'>
+                                                <div className='add-itemhead-left' style={{color:"#000"}}>{item.cardType}</div>
+                                                <svg width="12" height="12" viewBox="0 0 24 24" fill="none"><path d="M10 7L15 12L10 17" stroke="#333333ff" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+                                            </div>
+                                        </div>
+                                    ))
+                                  }
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
             </div>
             </div>
+        </div>
+        <div className='spss-section-6'>
+            <div className='spss-delivery-head'>Delivery details</div>
+            <div className='spss-set-deliloc-wrap'>
+                <div className='spss-setloc-icon'><svg width="16" height="16" viewBox="0 0 16 16" fill="none"><path d="M9.08414 13.8688L8.68438 13.4214L8.68438 13.4214L9.08414 13.8688ZM6.91586 13.8688L7.31562 13.4214L6.91586 13.8688ZM12.4 6.75C12.4 8.16783 11.8481 9.50544 11.0906 10.6696C10.3343 11.832 9.39653 12.7851 8.68438 13.4214L9.4839 14.3162C10.2422 13.6387 11.2624 12.606 12.0964 11.3241C12.9292 10.0441 13.6 8.47922 13.6 6.75H12.4ZM11.1113 3.63873C11.9364 4.46389 12.4 5.58305 12.4 6.75H13.6C13.6 5.26479 13.01 3.84041 11.9598 2.7902L11.1113 3.63873ZM8 2.35C9.16695 2.35 10.2861 2.81357 11.1113 3.63873L11.9598 2.7902C10.9096 1.74 9.48521 1.15 8 1.15V2.35ZM4.88873 3.63873C5.71389 2.81357 6.83305 2.35 8 2.35V1.15C6.51479 1.15 5.09041 1.74 4.0402 2.7902L4.88873 3.63873ZM3.6 6.75C3.6 5.58305 4.06357 4.46389 4.88873 3.63873L4.0402 2.7902C2.99 3.84041 2.4 5.26479 2.4 6.75H3.6ZM7.31562 13.4214C6.60347 12.7851 5.66569 11.832 4.90943 10.6696C4.15193 9.50544 3.6 8.16783 3.6 6.75H2.4C2.4 8.47922 3.07077 10.0441 3.90359 11.3241C4.73765 12.606 5.75779 13.6387 6.5161 14.3162L7.31562 13.4214ZM8 13.8964C7.94117 13.8964 7.89717 13.887 7.81199 13.833C7.69818 13.7607 7.56373 13.643 7.31562 13.4214L6.5161 14.3162C6.93321 14.6889 7.34507 15.0964 8 15.0964V13.8964ZM8.68438 13.4214C8.43627 13.643 8.30182 13.7607 8.18801 13.833C8.10283 13.887 8.05883 13.8964 8 13.8964V15.0964C8.65493 15.0964 9.06679 14.6889 9.4839 14.3162L8.68438 13.4214ZM8 9.85C9.71208 9.85 11.1 8.46208 11.1 6.75H9.9C9.9 7.79934 9.04934 8.65 8 8.65V9.85ZM4.9 6.75C4.9 8.46208 6.28792 9.85 8 9.85V8.65C6.95066 8.65 6.1 7.79934 6.1 6.75H4.9ZM8 3.65C6.28792 3.65 4.9 5.03792 4.9 6.75H6.1C6.1 5.70066 6.95066 4.85 8 4.85V3.65ZM11.1 6.75C11.1 5.03792 9.71208 3.65 8 3.65V4.85C9.04934 4.85 9.9 5.70066 9.9 6.75H11.1Z" fill="#333333ff"></path></svg></div>
+                <div className='spss-loc-notsetwrap'>Location not set</div>
+                <div className='spss-loc-notsetwrap spss-select-loc'>Select delivery location</div>
+                <svg width="12" height="12" viewBox="0 0 16 16" fill="none"><path d="M6 3L11 8L6 13" stroke="#1967f2ff" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+            </div>
+            <div className='spss-deliverypartner-wrap'>
+                <div className='spss-deliverypartner-name-wrap'>
+                    <div><svg width="16" height="16" fill="none" viewBox="0 0 24 24"><path d="M4.5 13.054v6.219c0 .192.079.378.22.514.14.136.33.213.53.213h13.5c.199 0 .39-.077.53-.213a.717.717 0 0 0 .22-.514v-6.219M5.063 4h13.875c.162.001.32.053.45.148s.225.228.271.38L21 9.09H3l1.34-4.564a.737.737 0 0 1 .272-.379.775.775 0 0 1 .45-.148v0Z" stroke="#333333ff" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"></path><path d="M9 9.09v1.456c0 .771-.316 1.511-.879 2.056A3.048 3.048 0 0 1 6 13.455a3.048 3.048 0 0 1-2.121-.853A2.865 2.865 0 0 1 3 10.546V9.09M15 9.09v1.456c0 .771-.316 1.511-.879 2.056a3.048 3.048 0 0 1-2.121.853 3.048 3.048 0 0 1-2.121-.853A2.865 2.865 0 0 1 9 10.546V9.09M21 9.09v1.456c0 .771-.316 1.511-.879 2.056a3.048 3.048 0 0 1-2.121.853 3.048 3.048 0 0 1-2.122-.853A2.865 2.865 0 0 1 15 10.546V9.09" stroke="#333333ff" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"></path></svg></div>
+                    <div>Fulfilled by HSAtlastradeFashion</div>
+                </div>
+                <div className='spss-seller-experience-wrap'>
+                    <div>{product.starAvg}</div>
+                    <div><svg width="12" height="12" viewBox="0 0 24 24" fill="#707070ff"><path d="m12.002 2.588 2.222 6.838h7.19l-5.816 4.227 2.222 6.839-5.818-4.227-5.817 4.227 2.222-6.84L2.59 9.427h7.19z" stroke="#707070ff" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"></path></svg></div>
+                    <div>•</div>
+                    <div>3 years with Flipkart</div>
+                </div>
+            </div>
+        </div>
+        <div className='spss-lightblue-line'></div>
+        <div className='spss-section-7'>
+            <div className='three-boxitem-wrap'>
+                <div className='three-box-item-inner'>
+                    <div className='three-box-columnwrap'>
+                        <svg width="32" height="32" viewBox="0 0 32 32" fill="none"><rect width="32" height="32" rx="8" fill="#fafafaff"></rect><path d="M17.6477 27.286C23.4199 27.286 28.0991 22.6068 28.0991 16.8346C28.0991 11.0624 23.4199 6.38318 17.6477 6.38318C11.8755 6.38318 7.19629 11.0624 7.19629 16.8346C7.19629 22.6068 11.8755 27.286 17.6477 27.286Z" fill="#cfebfeff"></path><path d="M26.6586 15.2602C26.6586 9.48807 21.9794 4.80884 16.2072 4.80884C10.4351 4.80884 5.75586 9.48807 5.75586 15.2602C5.75586 21.0323 10.4351 25.7116 16.2072 25.7116C18.4529 25.7116 20.5332 25.0033 22.2369 23.7979" stroke="#0a0a0aff" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M26.9529 16.6703C26.8206 16.8995 26.7545 17.014 26.6552 17.014C26.556 17.014 26.4898 16.8995 26.3575 16.6703L25.0102 14.3367C24.8779 14.1075 24.8118 13.993 24.8614 13.907C24.911 13.8211 25.0433 13.8211 25.3079 13.8211L28.0025 13.8211C28.2671 13.8211 28.3994 13.8211 28.4491 13.907C28.4987 13.993 28.4325 14.1075 28.3002 14.3367L26.9529 16.6703Z" fill="#0a0a0aff"></path><g clip-path="url(#ReturnPeriod_a)"><path d="M21.4395 11.8779L16.627 9.24467C16.4984 9.17362 16.3539 9.13635 16.207 9.13635C16.0601 9.13635 15.9156 9.17362 15.787 9.24467L10.9745 11.879C10.8371 11.9542 10.7224 12.0649 10.6423 12.1996C10.5623 12.3342 10.5199 12.4879 10.5195 12.6446V17.8749C10.5199 18.0316 10.5623 18.1853 10.6423 18.3199C10.7224 18.4546 10.8371 18.5653 10.9745 18.6405L15.787 21.2748C15.9156 21.3459 16.0601 21.3831 16.207 21.3831C16.3539 21.3831 16.4984 21.3459 16.627 21.2748L21.4395 18.6405C21.577 18.5653 21.6917 18.4546 21.7717 18.3199C21.8518 18.1853 21.8942 18.0316 21.8945 17.8749V12.6451C21.8945 12.4882 21.8522 12.3342 21.7721 12.1992C21.6921 12.0642 21.5772 11.9532 21.4395 11.8779ZM16.207 10.0103L20.6012 12.4165L18.9726 13.3074L14.5784 10.9012L16.207 10.0103ZM16.207 14.8228L11.8129 12.4165L13.6673 11.401L18.0615 13.8072L16.207 14.8228ZM21.0195 17.8771L16.6445 20.2719V15.5791L18.3945 14.6215V16.5728C18.3945 16.6888 18.4406 16.8001 18.5227 16.8822C18.6047 16.9642 18.716 17.0103 18.832 17.0103C18.9481 17.0103 19.0593 16.9642 19.1414 16.8822C19.2234 16.8001 19.2695 16.6888 19.2695 16.5728V14.1425L21.0195 13.1849V17.8749V17.8771Z" fill="#1967f2ff"></path></g><defs><clipPath id="ReturnPeriod_a"><rect width="14" height="14" fill="white" transform="translate(9.20703 8.26025)"></rect></clipPath></defs></svg>
+                        <div>10-Days Return</div>
+                    </div>
+                </div>
+            </div>
+            <div className='three-boxitem-wrap'>
+                <div className='three-box-item-inner'>
+                    <div className='three-box-columnwrap'>
+                        <svg width="32" height="32" viewBox="0 0 32 32" fill="none"><rect width="32" height="32" rx="8" fill="#fafafaff"></rect><rect x="5.89941" y="10.3398" width="19.5898" height="11.6088" rx="2.5278" fill="#cfebfeff"></rect><path d="M24.038 17.958V13.9443C24.038 11.561 24.038 10.3694 23.2976 9.62905C22.5573 8.88867 21.3657 8.88867 18.9824 8.88867H8.77825C6.39502 8.88867 5.20341 8.88867 4.46303 9.62905C3.72266 10.3694 3.72266 11.561 3.72266 13.9443V15.4419C3.72266 17.8251 3.72266 19.0167 4.46303 19.7571C5.20341 20.4975 6.39502 20.4975 8.77825 20.4975H17.8709H20.4103" stroke="#0a0a0aff" stroke-width="1.2" stroke-linecap="round"></path><path d="M11.8447 13.2661H16.4917" stroke="#1967f2ff" stroke-linecap="round" stroke-linejoin="round"></path><path d="M11.8447 11.4983H16.4917" stroke="#1967f2ff" stroke-linecap="round" stroke-linejoin="round"></path><path d="M13.1517 11.4983C13.6524 11.4983 14.1326 11.6972 14.4866 12.0512C14.8406 12.4053 15.0395 12.8854 15.0395 13.3861C15.0395 13.8868 14.8406 14.367 14.4866 14.721C14.1326 15.0751 13.6524 15.274 13.1517 15.274H11.8447L15.0395 18.1783" stroke="#1967f2ff" stroke-linecap="round" stroke-linejoin="round"></path><g clip-path="url(#NoCOD_a)"><path d="M26.875 16.0703L20.125 22.8203" stroke="#1967f2ff" stroke-width="3" stroke-linecap="square" stroke-linejoin="round"></path><path d="M26.875 22.8203L20.125 16.0703" stroke="#1967f2ff" stroke-width="3" stroke-linecap="square" stroke-linejoin="round"></path></g><defs><clipPath id="NoCOD_a"><rect width="12" height="12" fill="white" transform="translate(17.5 13.4453)"></rect></clipPath></defs></svg>
+                        <div>No cash on delivery</div>
+                    </div>
+                </div>
+            </div>
+            <div className='three-boxitem-wrap'>
+                <div className='three-box-item-inner'>
+                    <div className='three-box-columnwrap'>
+                        <svg width="32" height="32" viewBox="0 0 32 32" fill="none"><rect width="32" height="32" rx="8" fill="#fafafaff"></rect><path d="M25.7437 24.5112C25.2019 25.6109 21.536 28.5315 19.3098 30.06C18.7356 30.4543 17.9873 30.4545 17.3994 30.081C15.9286 29.1468 14.701 28.2614 13.6765 27.4365C10.4368 24.8278 8.54199 20.7999 8.54199 16.5329V10.9565C8.54199 10.0441 9.20261 9.27192 10.085 9.03998C13.0372 8.26395 15.9034 6.80871 17.3637 6.00154C17.9838 5.65877 18.7415 5.65877 19.3616 6.00154C20.8219 6.80871 23.6881 8.26395 26.6403 9.03998C27.5227 9.27192 28.1833 10.0441 28.1833 10.9565V16.5326C28.1833 19.4244 27.313 22.2064 25.7437 24.5112Z" fill="#cfebfeff"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M18.4737 20.3238H20.082C20.6593 20.3238 21.0717 19.8866 21.2366 18.8374C21.4016 17.8318 21.1954 17.3946 20.5768 17.3946H18.9685L19.216 15.9081C19.5046 14.2031 20.3294 13.3724 21.7315 13.3724H21.9789C22.7624 13.3724 23.1748 13.2412 23.216 12.8477C23.2573 12.5854 23.1748 12.2357 23.1335 11.8422C23.0511 11.4487 22.9686 11.099 22.8449 10.8804C22.6799 10.5306 22.2263 10.3557 21.5665 10.3557C19.917 10.3557 18.5562 10.8366 17.484 11.7985C16.4118 12.7603 15.752 14.0719 15.4221 15.8207L15.1747 17.3946H11.8344C11.7932 17.3946 8.288 17.7881 8.288 17.7881L11.5045 18.3127L11.6282 18.7936L9.69008 18.9685L11.752 19.2746L11.8344 19.668L7.21582 20.0615C7.21582 20.0615 12.3293 20.2801 12.3705 20.2801H14.6798L13.8188 28.0484L17.5302 28.4418L18.4737 20.3238Z" fill="#1967f2ff"></path><path d="M20.2336 26.4695C19.2105 27.289 18.6492 27.7238 17.4376 28.4832C16.8501 28.8514 16.1032 28.8507 15.5168 28.4806C14.0143 27.5322 12.7611 26.6335 11.716 25.7964C8.4249 23.1604 6.5 19.0904 6.5 14.7786V9.12866C6.5 8.2157 7.16143 7.44308 8.04495 7.21311C11.0657 6.42688 14.0011 4.94119 15.4819 4.12585C16.0998 3.78564 16.8533 3.78564 17.4711 4.12585C18.9519 4.94119 21.8874 6.42688 24.9081 7.21311C25.7916 7.44308 26.453 8.21571 26.453 9.12867V14.7784C26.453 17.7005 25.569 20.5115 23.9747 22.8405" stroke="#0a0a0aff" stroke-width="1.2" stroke-linecap="round"></path></svg>
+                        <div>Filpkart Assured</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className='spss-lightblue-line'></div>
+        <div className='spss-section-8'>
+           <div className='spss-product-hight-wrap'>
+            <div className=''>Product highlights</div>
+            <div>Key features, specifications and more</div>
+           </div>
+           <div className='spss-highlight-arrow-wrap'>
+            <svg width="16" height="16" viewBox="0 0 256 256"><path fill="none" d="M0 0h256v256H0z"></path><path fill="none" stroke="#333333ff" stroke-linecap="round" stroke-linejoin="round" stroke-width="16" d="m208 96-80 80-80-80"></path></svg>
+           </div>
+        </div>
+        <div className='spss-lightblue-line'></div>
+        <div className='spss-section-9'>
+           <div className='spss-product-hight-wrap'>
+            <div className=''>All details</div>
+            <div>Features, description and more</div>
+           </div>
+           <div className='spss-highlight-arrow-wrap'>
+            <svg width="16" height="16" viewBox="0 0 256 256"><path fill="none" d="M0 0h256v256H0z"></path><path fill="none" stroke="#333333ff" stroke-linecap="round" stroke-linejoin="round" stroke-width="16" d="m208 96-80 80-80-80"></path></svg>
+           </div>
         </div>
         <div className='fix'>
             <div className='bottom-fix-bt-wrap'>
---
+++ src/config/BankOffers.js
@@ -0,0 +1,59 @@
+const bankCards=[
+    [
+       {
+        cardImage:"https://rukminim1.flixcart.com/www/48/48/promos/01/09/2025/5a1f9675-770e-4043-8e1c-4f246b657998.png?q=60",
+        offer:"₹47 off",
+        cardName:"Filpkart Axis",
+        cardType:"Credit Card"
+       },
+       {
+        cardImage:"https://rukminim1.flixcart.com/www/48/48/promos/01/09/2025/5a1f9675-770e-4043-8e1c-4f246b657998.png?q=60",
+        offer:"₹47 off",
+        cardName:"Filpkart Axis",
+        cardType:"Debit Card"
+       }, 
+    ],
+    [
+        {
+        cardImage:"https://rukminim1.flixcart.com/www/48/48/promos/11/09/2025/812a123c-f432-42fc-9f80-502cd78ddaeb.png?q=60",
+        offer:"₹47 off",
+        cardName:"Filpkart SBI",
+        cardType:"Credit Card"
+       }, 
+       {
+        cardImage:"https://rukminim1.flixcart.com/www/48/48/promos/06/09/2025/562dc459-4ff2-4e87-8da4-f9f865db8be1.jpeg?q=60",
+        offer:"₹50 off",
+        cardName:"SuperMoney",
+        cardType:"Upi"
+       }, 
+    ],
+    [
+        {
+        cardImage:"https://rukminim1.flixcart.com/www/48/48/promos/30/08/2024/0eeba367-989d-49e6-a27b-d7884f43e63d.png?q=60",
+        offer:"₹30 off",
+        cardName:"Patym",
+        cardType:"UPI"
+       },
+       {
+        cardImage:"https://rukminim1.flixcart.com/www/48/48/promos/30/08/2024/17db8132-7d7a-423a-8d9e-8ffa588e4751.png?q=60",
+        offer:"₹30 off",
+        cardName:"BHIM",
+        cardType:"UPI"
+       }, 
+    ],
+    [
+        {
+        cardImage:"https://rukminim1.flixcart.com/www/48/48/promos/25/07/2024/40a2087b-81b3-4938-8afe-3565b82a66f0.png?q=60",
+        offer:"₹30 off",
+        cardName:"Multiple Banks",
+        cardType:"UPI"
+       },
+       {
+        cardImage:"https://rukminim1.flixcart.com/www/48/48/promos/16/09/2025/d2212a97-b7f9-4d9e-8636-0eb8079838b4.png?q=60",
+        offer:"₹30 off",
+        cardName:"CRED",
+        cardType:"UPI"
+       }
+    ]
+]
+export default bankCards
